ee67ccc0e7d33630b757ec959cfda1af7b35ddf8
Ahleizah Franks <ahleizahfranks@Ahleizahs-MacBook-Pro.local>
2017-04-13 11:58:03 -0500

    Edited Layout and Login

--- login.js
+++ login.js
@@ -1,11 +1,17 @@
 function login()
 {
-	if(   document.getElementById("text1").value == "test"
-	   && document.getElementById("text2").value == "test" )
+	if (document.getElementById("text1").value == "Admin" && document.getElementById("text2").value == "test")
+	{
+		alert( "validation succeeded" );
+		location.href="AdminDashboard.html";
+	}
+	
+	else if(document.getElementById("text1").value == "Resident" && document.getElementById("text2").value == "test")
 	{
 		alert( "validation succeeded" );
 		location.href="ResidentDashboard.html";
 	}
+	
 	else
 	{
 		alert( "validation failed" );
